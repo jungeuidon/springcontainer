3dc3a55324d9ee8b748bdb1c998b07160426b1d4
venture21 <park0122@gmail.com>
2019-10-14 11:55:50 +0900

    Spring-Test on JUnit #1

--- src/main/java/config/user/UserConfig02.java
+++ src/main/java/config/user/UserConfig02.java
@@ -1,19 +1,14 @@
 package config.user;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-import kr.co.itcen.springcontainer.user.Friend;
 import kr.co.itcen.springcontainer.user.User;
 
-public class UserConfig01 {
+@Configuration
+public class UserConfig02 {
 	@Bean
 	public User user() {
 		return new User();
 	}
-	
-	@Bean
-	public Friend friend() {
-		return new Friend("둘리");
-	}
-	
 }
